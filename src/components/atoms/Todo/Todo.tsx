210601ef8233ba4cfd1e4bf6c06c9fd92c0522e1
Igor Lysenko <proxly.pro@gmail.com>
2020-11-05 07:34:29 +0300

    add the ability to drag and drop tasks from the list to the list

--- src/components/atoms/Todo/Todo.tsx
+++ src/components/atoms/Todo/Todo.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { Button } from 'antd';
 import { useDispatch } from 'react-redux';
-import { DragDropContext, Droppable, OnDragEndResponder } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, OnDragEndResponder, DraggableLocation } from 'react-beautiful-dnd';
 
 // Components
 import List, { TodoListProps } from '@components/atoms/Todo/components/List';
@@ -23,6 +23,25 @@ const reorder = (
   const [removed] = items.splice(startIndex, 1);
   items.splice(endIndex, 0, removed);
   return { ...list, items };
+};
+
+const move = (
+  source: TodoListProps,
+  destination: TodoListProps,
+  droppableSource: DraggableLocation,
+  droppableDestination: DraggableLocation,
+) => {
+  const sourceClone = [...source.items];
+  const destClone = [...destination.items];
+
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  return {
+    [droppableSource.droppableId]: { ...source, items: sourceClone },
+    [droppableDestination.droppableId]: { ...destination, items: destClone },
+  };
 };
 
 export interface TodoProps {
@@ -53,6 +72,16 @@ const Todo: React.FC<TodoProps> = ({ className, lists }) => {
       );
 
       dispatch(TodoActions.updateList(list));
+    } else {
+      const lists = move(
+        getList(source.droppableId)!,
+        getList(destination.droppableId)!,
+        source,
+        destination,
+      );
+
+      dispatch(TodoActions.updateList(lists[source.droppableId]));
+      dispatch(TodoActions.updateList(lists[destination.droppableId]));
     }
   };
 
